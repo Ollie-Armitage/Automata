76e1147c07d07c509ab64567c6112888603e52d5
Ollie <oliverrarmitage@gmail.com>
2021-10-15 07:57:52 +0100

    Added abstract attachment module

---
+++ Automata/Modules/BaseModules/AttachmentModule.cs
@@ -0,0 +1,23 @@
+﻿using Discord;
+using Discord.Commands;
+
+namespace Automata.Modules.BaseModules
+{
+    public abstract class AttachmentModule : ModuleBase<SocketCommandContext>
+    {
+        public virtual void BeforeUpload()
+        {
+            // Mark Uploading
+        }
+
+        public virtual void AfterUpload()
+        {
+            // Mark Uploaded
+        }
+
+        public void Upload()
+        {
+            // Upload process should always be the same, not overridable
+        }
+    }
+}
